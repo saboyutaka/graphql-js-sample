41257ae6635eec402d7cc3dce682d9243fdf4d20
saboyutaka <yutaka.paradigm.shift@gmail.com>
2021-04-25 14:55:04 +0900

    add context

--- index.js
+++ index.js
@@ -11,16 +11,18 @@ const schema = buildSchema(`
 
 // The root provides a resolver function for each API endpoint
 const root = {
-  hello: () => {
+  hello: (parent, args, ctx, info) => {
     return 'Hello world!';
   },
 };
 
 const app = express();
-app.use('/graphql', graphqlHTTP({
+app.use('/graphql', graphqlHTTP(async (request) => ({
   schema: schema,
   rootValue: root,
+  context: request.headers,
   graphiql: true,
-}));
+})));
+
 app.listen(4000);
 console.log('Running a GraphQL API server at http://localhost:4000/graphql');
