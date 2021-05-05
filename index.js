aece878f4829bea6424ba84bbb70f86ce237a752
saboyutaka <yutaka.paradigm.shift@gmail.com>
2021-05-05 16:50:51 +0900

    makeExecutableSchema instead of RootResolver

--- index.js
+++ index.js
@@ -1,28 +1,64 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const { buildSchema } = require('graphql');
+const { makeExecutableSchema } = require('@graphql-tools/schema');
 
 // Construct a schema, using GraphQL schema language
-const schema = buildSchema(`
+const typeDefs = `
   type Query {
     hello: String
+    me: User
   }
-`);
 
-// The root provides a resolver function for each API endpoint
-const root = {
-  hello: (parent, args, ctx, info) => {
-    return 'Hello world!';
+  type User {
+    name: String!
+    email: String!
+    age: Int
+    address: Address
+  }
+
+  type Address {
+    prefecture: String!
+    postalCode: String!
+  }
+`
+
+const user = {
+  name: "hoge",
+  email: "hoge@hoge.com",
+  age: 25,
+}
+
+const address = {
+  prefecture: "沖縄県",
+  postalCode: "9040004"
+}
+
+const resolvers = {
+  Query: {
+    hello: (parent, args, ctx, info) => 'Hello world!',
+    me: (parent, args, ctx, info) => user,
+  },
+
+  User: {
+    address: () => address,
   },
 };
 
+const schema = makeExecutableSchema({ typeDefs, resolvers })
+
+
 const app = express();
-app.use('/graphql', graphqlHTTP(async (request) => ({
-  schema: schema,
-  rootValue: root,
-  context: request.headers,
-  graphiql: true,
-})));
+app.use('/graphql', graphqlHTTP(async (request) => {
+  const { accept, cookie, } = request.headers
+  const user = { sub: "1" }
+  const ctx = { accept, cookie, user }
+
+  return {
+    schema: schema,
+    context: ctx,
+    graphiql: true,
+  }
+}));
 
 app.listen(4000);
 console.log('Running a GraphQL API server at http://localhost:4000/graphql');
